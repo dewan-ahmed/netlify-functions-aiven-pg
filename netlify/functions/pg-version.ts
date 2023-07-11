077c8bd0ea308f8273469dce05a318a7555b3035
Dewan Ishtiaque Ahmed <dewan.ahmed@aiven.io>
2023-07-11 13:32:06 -0300

    Update

--- netlify/functions/pg-version.ts
+++ netlify/functions/pg-version.ts
@@ -1,10 +1,29 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import { Client } from "pg";
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Your PG version is: 15." }),
-  };
+  const client = new Client({
+    connectionString: process.env.POSTGRES_URI,
+    ssl: true, // Enable SSL if required by your PostgreSQL instance
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query("SELECT version();");
+    const postgresVersion = result.rows[0].version;
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Your PG version is: ${postgresVersion}.` }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Error connecting to PostgreSQL" }),
+    };
+  } finally {
+    await client.end();
+  }
 };
 
 export { handler };
